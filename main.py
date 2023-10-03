aefa9b2cc122eb60fe2c66a551af7178fef65ff7
Pierre_Chambet <pierrechambet@gmail.com>
2023-10-03 18:10:27 +0200

    Utilisation du module sprite

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ ECRAN = pygame.display.set_mode((LARGEUR, HAUTEUR)) #Afficher l'ecran de jeu
 CIRCUIT = pygame.image.load("circuit.jpeg")
 CAR = pygame.image.load("car.png")
 
+class Car(pygame.sprite.Sprite): #Utilisation de la classe "Sprite" du module "sprite"
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(os.path("car.png"))
