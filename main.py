9ef4d083077b81d5f860bc8983187678706d5b54
Pierre_Chambet <pierrechambet@gmail.com>
2023-10-03 17:35:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pygame
+import os
+
+LARGEUR = 1500 #dimension de l'image circuit
+HAUTEUR = 1000
+
+ECRAN = pygame.display.set_mode((LARGEUR, HAUTEUR)) # Afficher l'ecran de jeu
+
 
 CIRCUIT = pygame.image.load("circuit.png")
 CAR = pygame.image.load("car.png")
