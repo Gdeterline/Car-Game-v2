98fa18750760927b752b604b84bef82f98b6642c
Pierre_Chambet <pierrechambet@gmail.com>
2023-10-17 19:22:55 +0200

    Avancée sur la rotation
    La voiture tourne de manière centrée sur elle-même et se place sur le côtés simultanément.
    Il faut maintenant qu'elle se déplace vers la direction voulue.
    Il faudra pour cela changer le vector nommé Vector2 et effectuant des transformations sur ses coordonnées (x,y)

--- car.py
+++ car.py
@@ -4,23 +4,27 @@ import sys
 import numpy as np
 
 CAR = pygame.image.load(os.path.join(os.getcwd(), "./car.png"))
+
 # permet de réduire l'image unifomement plus tard
 w = CAR.get_width()
 h = CAR.get_height()
 
+x = 200
+y = 200
 class Car(pygame.sprite.Sprite): #Utilisation de la classe "Sprite" du module "sprite". Car defined by extending Sprite
     def __init__(self):
         super().__init__() #initialiser la classe
         self.image = pygame.transform.scale(CAR, (w * 0.1, h * 0.1))
         #genere rectangle de coordonnees (x, y) de la taille de self.image
-        self.rect = self.image.get_rect(center=(200, 200)) 
-        #vecteur à dimensions de coordonnees (x, y) ; variable vitesse = direction de la voiture
+        self.rect = self.image.get_rect(center=(x, y)) 
+        #vecteur à dimensions de coordonnees (x, y) ; variable vitesse (= direction de la voiture)
         self.vel = pygame.math.Vector2(1, 0) 
         #variable pour l'angle
         self.angle = 0
         #vitesse de rotation
         self.angle_vel = 1
-        
+        self.rotated_image = self.image
+        self.new_rect = self.rect
         
     # driving function
     def drive(self):
@@ -34,31 +38,26 @@ class Car(pygame.sprite.Sprite): #Utilisation de la classe "Sprite" du module "s
     def steer(self):
         steer_keys = pygame.key.get_pressed()
         
- # test déplacement / rotation avec K_RIGHT       
+        # test déplacement / rotation avec K_RIGHT
+        
         if steer_keys[pygame.K_RIGHT]:
-            self.angle += self.angle_vel
+            self.angle -= self.angle_vel
+            self.rotated_image = pygame.transform.rotate(self.image, self.angle)
+            new_rect = self.rotated_image.get_rect(center=(x,y))
             
-            self.image2 = pygame.transform.rotate(self.image, -self.angle/10)
-            
-            #self.rect.y += self.vel[0] * 2
+            #self.rect.y += self.vel[0]
             #self.vel[1] = self.vel[1] + self.angle_vel
             #self.vel.rotate_ip(self.angle_vel)
-            
-# test vitesse / vecteur avec K_LEFT
+
+        # test vitesse / vecteur avec K_LEFT
         elif steer_keys[pygame.K_LEFT]:
-            self.angle -= self.angle_vel
+            self.angle += self.angle_vel
+            self.rotated_image = pygame.transform.rotate(self.image, self.angle)
+            new_rect = self.rotated_image.get_rect(center=(x,y))
+            #self.rect.y -= self.vel[0]
             
-            self.rect.y -= self.vel[0] * 2
-            
-            #self.image = pygame.transform.rotate(self.image, +self.angle/3)
-            #self.vel[1] = self.vel[1] + self.angle_vel
-            #self.vel.rotate_ip(-self.angle_vel)
-            
-        #self.image = pygame.transform.rotozoom(self.image, self.angle, 0.1)
-        #self.rect = self.image.get_rect(center = self.rect.center)
-        
     # update driving and steering (which will depend on the user inputs)
     def update(self):
         self.drive()
         self.steer()
-    
+        
--- main.py
+++ main.py
@@ -20,14 +20,14 @@ car = car.Car()
 # Run until user quits window
 running = True
 while running:
-    # check if user quits
+    
     for event in pygame.event.get():
-        # player quits the window
+        
         if event.type == pygame.QUIT:
             running = False
-        # does he click on a key ?
+        
         elif event.type == pygame.KEYDOWN:
-            # he clicks on the escape key
+            
             if event.key == pygame.K_ESCAPE:
                 running = False
     
@@ -36,17 +36,17 @@ while running:
 
     # display race track on the screen with .blit()
     screen.blit(CIRCUIT, (0, 0))
-        
+
     # updating the position of the car
     car.update()
     
     # displays the car on the track
-    screen.blit(car.image, car.rect)
-    screen.blit(car.image2, car.rect)
+    
+    screen.blit(car.rotated_image, car.new_rect)
     
     # update the display
     pygame.display.update()
+    pygame.display.flip()
         
- 
 # Quit pygame when done playing
 pygame.quit()
