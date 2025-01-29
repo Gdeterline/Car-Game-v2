9e2b499f90e6e56f8e53e356999a14310b44d1f6
Guillaume de Terline <guillaume.macquart_de_terline@telecom-sudparis.eu>
2025-01-29 16:46:13 +0100

    AIMainLoop class created - track generation finalized

--- Genetic_Algorithm_Car/AIMainLoop.py
+++ Genetic_Algorithm_Car/AIMainLoop.py
@@ -1,33 +1,51 @@
-# %%
 import pygame
 from Track import Track
+from Colors import Color
 
 pygame.init()
 
-# %%
 #######################   Testing Racetrack drawing functionnality   #######################
 #######################   Works just as expected - Good news!   ############################
 
-track = Track(1200, 800, 25)
-track.surface = pygame.display.set_mode((track.width, track.height))
+class AIMainLoop():
 
-running = True
-while running:
+    def __init__(self):
+        self.track = Track(width=1200, height=800, brush_size=40, background_color=Color.BLACK, track_color=Color.WHITE)
+        self.track.surface = pygame.display.set_mode((self.track.width, self.track.height))
 
-    clock = pygame.time.Clock()
-    for event in pygame.event.get():    
-        if event.type == pygame.QUIT:
-            running = False
-            track.save_track()
-            break
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-                track.save_track()
-                break
+    def TrackLoop(self):
+        """
+        TrackLoop function is in charge of the track generation
+        """
+        track_running = True
+        while track_running:
 
-    track.user_track_drawing()
-    pygame.display.flip()
+            clock = pygame.time.Clock()
 
-    clock.tick(60)
-# %%
+            for event in pygame.event.get():    
+                if event.type == pygame.QUIT:
+                    track_running = False
+                    break
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        track_running = False
+                        break
+                    elif event.key == pygame.K_s:
+                        pygame.image.save(self.track.surface, "./Genetic_Algorithm_Car/assets/racetracks/Racetrack.png")
+                        print("Track Saved")
+
+            self.track.user_track_drawing()
+            pygame.display.flip()
+
+            clock.tick(60)
+
+    def MainLoop(self):
+        """
+        MainLoop function is in charge of the driving simulation itself
+        """
+        pass
+
+
+
+loop = AIMainLoop()
+loop.TrackLoop()
--- Genetic_Algorithm_Car/Track.py
+++ Genetic_Algorithm_Car/Track.py
@@ -2,12 +2,12 @@ import pygame
 from Colors import Color
 
 class Track():
-    def __init__(self, width, height, brush_size):
+    def __init__(self, width, height, brush_size, background_color, track_color):
             self.width = width
             self.height = height
             self.surface = pygame.Surface((width, height))
-            self.surface.fill(Color.WHITE)
-            self.track_color = Color.BLACK
+            self.surface.fill(background_color)
+            self.track_color = track_color
             self.brush_size = brush_size
 
 
@@ -15,8 +15,6 @@ class Track():
         if pygame.mouse.get_pressed()[0]:
             pygame.draw.circle(self.surface, self.track_color, pygame.mouse.get_pos(), self.brush_size)
         elif pygame.mouse.get_pressed()[2]:
-            pygame.draw.circle(self.surface, Color.WHITE, pygame.mouse.get_pos(), self.brush_size)
+            pygame.draw.circle(self.surface, Color.BLACK, pygame.mouse.get_pos(), self.brush_size)
 
-    def save_track(self):
-        pygame.image.save(self.surface, "./Racetrack.png")
 
