77651b0a3e3b88bcb089c687fe338a25a1c9c696
Guillaume de Terline <guillaume.macquart_de_terline@telecom-sudparis.eu>
2025-01-30 11:21:00 +0100

    track generation + starting position is fully complete

--- Genetic_Algorithm_Car/AIMainLoop.py
+++ Genetic_Algorithm_Car/AIMainLoop.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 from Track import Track
 from Colors import Color
@@ -5,11 +6,6 @@ from Colors import Color
 pygame.init()
 
 font = pygame.font.SysFont("Calibri", 18)
-txtsurf1 = font.render("Left click to draw the track in white", True, Color.WHITE)
-txtsurf2 = font.render("Right click to erase track marks", True, Color.WHITE)
-txtsurf3 = font.render("Press s key to save the track", True, Color.WHITE)
-txtsurf4 = font.render("Press Return to place Car Starting Point", True, Color.WHITE)
-txtsurf5 = font.render("Left click to draw the track in white", True, Color.WHITE)
 
 #######################   Testing Racetrack drawing functionnality   #######################
 #######################   Works just as expected - Good news!   ############################
@@ -19,6 +15,7 @@ class AIMainLoop():
     def __init__(self):
         self.track = Track(width=1200, height=800, brush_size=40, background_color=Color.BLACK, track_color=Color.WHITE)
         self.track.surface = pygame.display.set_mode((self.track.width, self.track.height))
+        self.instruction_bar = pygame.Rect(0, 0, self.track.width, 80)
 
     def TrackLoop(self):
         """
@@ -37,29 +34,22 @@ class AIMainLoop():
                     if event.key == pygame.K_ESCAPE:
                         track_running = False
                         break
-                    elif event.key == pygame.K_s:
-                        pygame.image.save(self.track.surface, "./Genetic_Algorithm_Car/assets/racetracks/Racetrack.png")
-                        pygame.display.set_caption("Track Saved")
                     elif event.key == pygame.K_RETURN:
                         track_running = False
                         pygame.display.set_caption("Placing Starting Line")
-                        self.MainLoop()
+                        self.track.surface.fill(Color.BLACK, self.instruction_bar)
+                        self.StartingPosLoop()
 
-
-            self.track.surface.blit(txtsurf1,(10, txtsurf1.get_height() // 2))
-            self.track.surface.blit(txtsurf2,(10, txtsurf1.get_height() // 2 + 20))
-            self.track.surface.blit(txtsurf3,(10, txtsurf1.get_height() // 2 + 40))
-            self.track.surface.blit(txtsurf4,(self.track.width - txtsurf4.get_width() // 2 - 150, txtsurf4.get_height() // 2))
-
-
+            self.track.separation_line()
+            self.track.blit_text_track_drawing()
             self.track.user_track_drawing()
             pygame.display.flip()
 
             clock.tick(60)
 
-    def MainLoop(self):
+    def StartingPosLoop(self):
         """
-        MainLoop function is in charge of the driving simulation itself
+        StartingPosLoop function is in charge of setting the starting line (and the starting point, at the middle of the starting line)
         """
         running = True
         while running:
@@ -68,28 +58,56 @@ class AIMainLoop():
 
             for event in pygame.event.get():    
                 if event.type == pygame.QUIT:
-                    track_running = False
+                    running = False
                     break
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
-                        track_running = False
+                        running = False
                         break
+                    elif event.key == pygame.K_RETURN:
+                        running = False
+                        self.track.surface.fill(Color.BLACK, self.instruction_bar)
+                        pygame.image.save(self.track.surface, "./Genetic_Algorithm_Car/assets/racetracks/Racetrack.png")
+                        pygame.display.set_caption("Track Saved")
+                        pygame.time.wait(500)
+                        pygame.display.set_caption("Begin simulation")
+                        self.MainLoop()
 
-            self.track.surface.blit(txtsurf5,(10, txtsurf5.get_height() // 2))
-            
-            """
-            # To debug starting position - works just fine
 
-            if self.track.starting_position != []:
-                print(self.track.starting_position)
-            """
+            self.track.separation_line()
+            self.track.blit_test_starting_line()
             
-            # Need to debug starting line - three clicks then bugs
-            self.track.user_track_starting_line()
+            if self.track.user_track_starting_line() == True:
+                self.track.user_starting_position()
+
             pygame.display.flip()
 
             clock.tick(60)
 
 
+    def MainLoop(self):
+        """
+        MainLoop function is in charge of the Self Driving Car simulation
+        """
+        running = True
+        while running:
+
+            for event in pygame.event.get():    
+                if event.type == pygame.QUIT:
+                    running = False
+                    break
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+                        break
+
+            self.selected_circuit = pygame.image.load("./Genetic_Algorithm_Car/assets/racetracks/Racetrack.png")
+            self.track.surface.fill((0, 0, 0))
+            self.track.surface.blit((self.selected_circuit), (0, 0))   
+
+            pygame.display.flip()
+
+
+
 loop = AIMainLoop()
-loop.TrackLoop()
+loop.MainLoop() # To avoid having to build the track every time ^^
--- Genetic_Algorithm_Car/Track.py
+++ Genetic_Algorithm_Car/Track.py
@@ -1,6 +1,8 @@
 import pygame
 from Colors import Color
 
+pygame.init()
+font = pygame.font.SysFont("Calibri", 18)
 
 
 class Track():
@@ -11,11 +13,22 @@ class Track():
             self.surface.fill(background_color)
             self.track_color = track_color
             self.brush_size = brush_size
-            self.starting_position = []
             self.pos1 = tuple
             self.pos2 = tuple
-            self.click = 0
+            self.starting_position = tuple
+            self.spos1 = 0
+            self.spos2 = 0
 
+    def separation_line(self):
+        pygame.draw.line(self.surface, Color.GREY, start_pos=(0, 80), end_pos=(self.width, 80))
+
+    def blit_text_track_drawing(self):
+        txtsurf1 = font.render("Left click to draw the track in white", True, Color.WHITE)
+        txtsurf2 = font.render("Right click to erase track marks", True, Color.WHITE)
+        txtsurf4 = font.render("PRESS RETURN TO PLACE STARTING LINE", True, Color.WHITE)
+        self.surface.blit(txtsurf1,(10, txtsurf1.get_height() // 2))
+        self.surface.blit(txtsurf2,(10, txtsurf1.get_height() // 2 + 20))
+        self.surface.blit(txtsurf4,(self.width - txtsurf4.get_width() // 2 - 150, txtsurf4.get_height() // 2))
 
     def user_track_drawing(self):
         if pygame.mouse.get_pressed()[0]:
@@ -35,7 +48,13 @@ class Track():
             self.starting_position = pygame.mouse.get_pos()
             pygame.display.set_caption("Starting Position Set")
 
-
+    def blit_test_starting_line(self):
+        txtsurf5 = font.render("Left click to place first point of the starting line", True, Color.WHITE)
+        txtsurf6 = font.render("Right click to place second point of the starting line", True, Color.WHITE)
+        txtsurf4 = font.render("PRESS RETURN TO SAVE TRACK AND BEGIN SIMULATION", True, Color.WHITE)
+        self.surface.blit(txtsurf5,(10, txtsurf5.get_height() // 2))
+        self.surface.blit(txtsurf6,(10, txtsurf5.get_height() // 2 + 20))
+        self.surface.blit(txtsurf4,(self.width - txtsurf4.get_width() // 2 - 220, txtsurf4.get_height() // 2))
 
     def user_track_starting_line(self):
         if pygame.mouse.get_pressed()[0]:
@@ -45,21 +64,14 @@ class Track():
             pygame.draw.circle(self.surface, Color.RED, pygame.mouse.get_pos(), 2)
             self.pos2 = pygame.mouse.get_pos()
             pygame.draw.line(self.surface, Color.RED, start_pos=self.pos1, end_pos=self.pos2, width=2)
-            self.starting_position = tuple((self.pos1[0] - self.pos2[0], self.pos1[1] - self.pos2[1]))
-            print(self.starting_position)
-            pygame.draw.circle(self.surface, Color.BLUE, self.starting_position, 2)
-            return
+            return True
+
+    def user_starting_position(self):
+        self.starting_position = (self.spos1, self.spos2)
+        self.spos1 = (self.pos1[0] + self.pos2[0])/2
+        self.spos2 = (self.pos1[1] + self.pos2[1])/2
+        pygame.draw.circle(self.surface, Color.BLUE, self.starting_position, 2)
 
 
-            """
-        if pygame.mouse.get_pressed()[0]:
-            self.starting_line.append(pygame.mouse.get_pos())
-            pygame.draw.circle(self.surface, Color.RED, pygame.mouse.get_pos(), 2)
 
-        if len(self.starting_line) == 2:
-            pygame.draw.line(self.surface, Color.RED, start_pos=self.starting_line[0], end_pos=self.starting_line[1], width=2)
-            self.starting_position = tuple((self.starting_line[0][1] - self.starting_line[0][0], self.starting_line[1][1] - self.starting_line[1][0]))
-            pygame.draw.circle(self.surface, Color.BLUE, self.starting_position, 2)
-            print("Starting Line placed")
-            """
             
